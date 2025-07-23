f7a6d5416456df1f6b218377070144de1bd274c8
Fadloulahi14 <fallou.ndiaye22@isep-thies.edu.sn>
2025-07-23 00:06:19 +0000

    verion002

--- app/core/Database.php
+++ app/core/Database.php
@@ -19,7 +19,9 @@ class Database
         if (self::$connection === null) {
             try {
                 self::$connection = new PDO(
-                     'pgsql:host=postgres_appdaf;port=5432;dbname=appdaff', 'appdaf_user', 'secretpass',
+                     'pgsql:host=dpg-d1v73m6r433s73fe0a3g-a;port=5432;dbname=postgres_biin', 'postgres_biin_user', 'f3cOaIs9MHpE5F9YG0oVNV98YgMpyuWk',
+                    //  'pgsql:host=postgres_appdaf;port=5432;dbname=appdaff', 'appdaf_user', 'secretpass',                     'pgsql:host=postgres_appdaf;port=5432;dbname=appdaff', 'appdaf_user', 'secretpass',
+
                     [
                         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
